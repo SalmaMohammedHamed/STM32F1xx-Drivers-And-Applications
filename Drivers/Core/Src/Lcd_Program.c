9f01a9c077c36e17e5c6422a2df2eb6857a03fb4
SalmaMohammedHamed <Salma.Mustafa02@eng-st.cu.edu.eg>
2024-02-20 17:49:10 +0200

    Drivers till EXTI

---
+++ Drivers/Core/Src/Lcd_Program.c
@@ -0,0 +1,154 @@
+/*
+ * Lcd_Program.c
+ *
+ *  Created on: Sep 15, 2023
+ *      Author: salma
+ */
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "definition.h"
+#include "Special_Functions.h"
+#include "GPIO_interface.h"
+#include "Lcd_Interface.h"
+#include "Lcd_Private.h"
+#include "Lcd_config.h"
+
+void LCD_voidSendCommand(u8 command)
+{
+
+	//set the RS value as Low (command =>RS =  0 )
+		GPIO_voidSetPinValue(Lcd_ControlGroup,Lcd_RsPin,Low);
+	//set the RW value as Low (Write  => RW = 0 )
+		GPIO_voidSetPinValue(Lcd_ControlGroup,Lcd_RwPin,Low);
+	//set the E Value as High (High CLK)
+		GPIO_voidSetPinValue(Lcd_ControlGroup,Lcd_EPin,High);
+	//Send Command
+		GPIO_voidSetPortValue(Lcd_DCGroup,command);
+	//Set the E value as Low (Low CLK)
+		GPIO_voidSetPinValue(Lcd_ControlGroup,Lcd_EPin,Low);
+
+
+}
+
+void LCD_voidSendData(u8 data)
+{
+
+	//set the RS value as Low (Data =>RS =  1 )
+	GPIO_voidSetPinValue(Lcd_ControlGroup,Lcd_RsPin,High);
+	//set the RW vlaue as Low (Write  => RW = 0 )
+	GPIO_voidSetPinValue(Lcd_ControlGroup,Lcd_RwPin,Low);
+	//set the E Value as High (High CLK)
+	GPIO_voidSetPinValue(Lcd_ControlGroup,Lcd_EPin,High);
+	//Send Data
+	GPIO_voidSetPortValue(Lcd_DCGroup,data);
+	//Set the E value as Low (Low CLK)
+	GPIO_voidSetPinValue(Lcd_ControlGroup,Lcd_EPin,Low);
+
+
+}
+
+
+void LCD_voidInit()
+{
+	//set Direction
+	//all portA output
+	GPIO_voidSetPortDir(Lcd_ControlGroup, 0x11111111, 0x11111111);
+	//wait for 30ms
+	delay(35);
+	//sent the Function set
+	LCD_voidSendCommand(LCD_Set8Bit2Line5x8);
+	//wait for 39us
+	delay(1);
+	//sent the display
+	LCD_voidSendCommand(LCD_8BitCursorBlinkON);
+	//wait for 39us
+	delay(1);
+	//sent the Clear
+	LCD_voidSendCommand(LCD_8BitClearDisplay);
+	//wait for 1.53ms
+	delay(2);
+	//sent the EntryMode
+	LCD_voidSendCommand(LCD_8BitEntryMode1);
+
+
+}
+
+
+void LCD_voidSendString(u8 String[])
+{
+	u8 i = 0 ;
+	while(String[i]!='\0')
+	{
+		LCD_voidSendData(String[i]);
+		i++;
+	}
+}
+
+
+void LCD_voidSetPos(u8 x, u8 y)
+{
+	u8 adress;
+	if (x==first_line_pos)
+	{
+		adress=y+first_line_add;
+	}
+	else if (x==sec_line_pos)
+	{
+		adress=y+sec_line_add;
+	}
+	LCD_voidSendCommand(adress|LCD_SetAddressDDRAM);
+}
+
+void LCD_voidSpecialCharacter (u8 block_no , u8 *special, u8 x, u8 y)
+{
+	u8 adress;
+	u8 counter;
+	adress = block_no*8;
+	LCD_voidSendCommand(adress|LCD_SetAddressCGRAM);
+	for (counter=0; counter<8;counter++)
+	{
+		LCD_voidSendData(special[counter]);
+	}
+	//go back to DDRAM
+	LCD_voidSetPos(x,y);
+	LCD_voidSendData(block_no);
+}
+
+void LCD_voidClear()
+{
+	LCD_voidSendCommand(LCD_8BitClearDisplay);
+	//wait for 1.53ms
+	delay(2);
+}
+void LCD_voidSendNumber(s32 num) {
+    s8 counter;
+    u8 length = 0;
+    u8 StringStart = 0;
+    u8 IsNegative = 0;
+    u8 str[20];
+
+    if (num < 0) {
+        num = -num;
+        IsNegative = 1;
+        StringStart = 1;
+        str[0] = '-';
+        length++;
+    }
+
+    u32 temp_num = num;
+    do {
+        temp_num /= 10;
+        length++;
+    } while (temp_num > 0);
+
+    for (counter = length - 1; counter >= StringStart; counter--) {
+        str[counter] = num % 10 + '0';
+        num /= 10;
+    }
+    str[length] = '\0';
+    LCD_voidSendString(str);
+}
+
+
+
+
---
+++ Drivers/Core/Src/main.c
@@ -0,0 +1,57 @@
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "BIT_MATH.h"
+#include "STD_TYPES.h"
+#include "Special_Functions.h"
+#include "RCC_interface.h"
+#include "GPIO_interface.h"
+#include "GPIO_private.h"
+#include "NVIC_interface.h"
+#include "EXTI_interface.h"
+#include "EXTI_config.h"
+#include "EXTI_private.h"
+#include "AFIO_interface.h"
+#include "AFIO_private.h"
+#include "SysTick_interface.h"
+#include "Led_Interface.h"
+#include "Switch_Interface.h"
+#include "Lcd_Interface.h"
+
+void led (void)
+{
+	static u32 state=0;
+	static u32 counter=0;
+	if (counter%3000==0)
+	{
+		if (state==0)
+		{
+			GPIO_voidSetPinValue(GPIO_PORTA , GPIO_PIN0 , HIGH );
+			state=1;
+		}
+		else if (state==1)
+		{
+			GPIO_voidSetPinValue(GPIO_PORTA , GPIO_PIN0 , LOW );
+			state=0;
+		}
+
+	}
+
+	counter++;
+}
+
+int main(void)
+{
+
+	RCC_voidInitSysClock();
+	RCC_voidEnableClock(RCC_APB2,RCC_APP2_IOPA_EN);
+	GPIO_voidSetPinDir(GPIO_PORTA, GPIO_PIN0, GPIO_OUTPUT_PP_2MHZ);
+	STK_Init();
+	STK_SetIntervalPeriodic(16000000, led);
+
+
+  while (1)
+  {
+
+  }
+
+}
